f1033eaf51e07fd484e5360ee45ffe729197ddc4
Leonardo Blautzik <leoblautzik@gmail.com>
2018-05-29 15:21:17 -0300

    Add files via upload

--- KmasPequenios.java
+++ KmasPequenios.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -7,29 +8,45 @@ import java.util.TreeSet;
 
 public class KmasPequenios {
 	
+	private int cantEnteros;
+	private int cantPequenios;
+	private TreeSet<Integer> enteros = new TreeSet<Integer>();
+
+	String t="enterosPequenios.in";
+	
+	public KmasPequenios(String t) throws FileNotFoundException{
+		Scanner sc = new Scanner(new File(t));
+		this.cantEnteros=sc.nextInt();
+		this.cantPequenios=sc.nextInt();
+		
+		for(int i=0;i<cantEnteros;i++)
+			this.enteros.add(sc.nextInt());		
+
+		sc.close();
+
+	}
+	
+	public void resolver(String t) throws IOException{
+		PrintWriter s = new PrintWriter(new FileWriter(t)); 
+		s.println(cantPequenios);
+		for(int i=0;i<cantPequenios;i++){
+			s.println(enteros.first());	
+			enteros.remove(enteros.first());
+		}
+		s.close();
+	}
+	
 	public static void main(String[] args) throws IOException {
-			int cantEnteros;
-			int cantPequenios;
+		
+		KmasPequenios kmp = new KmasPequenios("enterosPequenios.in");
+		kmp.resolver("pequenios.out");
 			
-			TreeSet<Integer> enteros = new TreeSet<Integer>();
-
-			String t="enterosPequenios.in";
-
-			Scanner sc = new Scanner(new File(t));
-			cantEnteros=sc.nextInt();
-			cantPequenios=sc.nextInt();
-			for(int i=0;i<cantEnteros;i++)
-				enteros.add(sc.nextInt());		
-
-			sc.close();
 			
-			PrintWriter s = new PrintWriter(new FileWriter("pequenios.out")); 
-			s.println(cantPequenios);
-			for(int i=0;i<cantPequenios;i++){
-				s.println(enteros.first());	
-				enteros.remove(enteros.first());
-			}
-			s.close();
+			
+
+			
+			
+			
 	}
 	
 }
